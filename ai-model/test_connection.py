c2eda9ca1b5d980a1aecd62f8afe5fd330cecc74
Priyanshu-Mehrotra2704 <147127911+Priyanshu-Mehrotra2704@users.noreply.github.com>
2026-07-03 01:48:58 +0530

    trying to integrate supabase

---
+++ ai-model/test_connection.py
@@ -0,0 +1,13 @@
+# test_connection.py
+
+from database import engine
+
+try:
+    conn = engine.connect()
+
+    print("Connected Successfully!")
+
+    conn.close()
+
+except Exception as e:
+    print(e)
