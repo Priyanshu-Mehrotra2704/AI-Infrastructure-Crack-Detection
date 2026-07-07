a18ae78d302422953f90e1547d162333d758a4e8
Priyanshu-Mehrotra2704 <147127911+Priyanshu-Mehrotra2704@users.noreply.github.com>
2026-07-07 23:36:30 +0530

    Update History.jsx

--- frontend/src/components/History.jsx
+++ frontend/src/components/History.jsx
@@ -5,7 +5,7 @@ import { FaTrash } from "react-icons/fa";
 function History({ refresh }) {
 
     const [history, setHistory] = useState([]);
-    
+    const [search, setSearch] = useState("");
     const fetchHistory = async () => {
 
         try {
@@ -50,6 +50,21 @@ function History({ refresh }) {
                 Inspection History
 
             </h2>
+            <div className="flex justify-between items-center mb-6">
+
+        <h2 className="text-2xl font-bold">
+            Inspection History
+        </h2>
+
+            <input
+            type="text"
+            placeholder="Search image..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="border border-gray-300 rounded-lg px-4 py-2 w-72 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+
+        </div>
 
             {
                 history.length === 0 ?
