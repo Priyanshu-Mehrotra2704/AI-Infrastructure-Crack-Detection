c856a7e36e758348705ee57f74ce12938766c1d1
Priyanshu-Mehrotra2704 <147127911+Priyanshu-Mehrotra2704@users.noreply.github.com>
2026-08-01 23:15:02 +0530

    some minor changes

--- backend/model_loader.py
+++ backend/model_loader.py
@@ -1,10 +1,7 @@
-import os
 from tensorflow.keras.models import load_model
-
+import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 MODELS = {
-
     "Pavement": load_model(
         os.path.join(
             BASE_DIR,
@@ -12,7 +9,8 @@ MODELS = {
             "ai-model",
             "models",
             "best_model3.keras"
-        )
+        ),
+        compile=False
     ),
 
     "Wall": load_model(
@@ -22,7 +20,8 @@ MODELS = {
             "ai-model",
             "models",
             "wall_best_model.keras"
-        )
+        ),
+        compile=False
     ),
 
     "Deck": load_model(
@@ -32,9 +31,7 @@ MODELS = {
             "ai-model",
             "models",
             "deck_best_model_focal.keras"
-        )
+        ),
+        compile=False
     )
-
 }
-
-
